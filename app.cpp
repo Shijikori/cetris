2aaeca0367e3264b986feb205acc700a863e27a7
Shijikori <jeremi.campagna@gmail.com>
2021-09-04 22:20:42 -0400

    built window with shapes and monitor specs

--- app.cpp
+++ app.cpp
@@ -16,6 +16,9 @@ int main(int argc, char *argv[])
 
     // Create window
     window = glfwCreateWindow(640, 480, "GLApp", NULL, NULL);
+    glfwSetWindowMonitor(window, NULL, 0, 0, 0, 0, 60);
+    glfwSetWindowAttrib(window, GLFW_RESIZABLE, GLFW_FALSE);
+
     if (!window)
     {
         glfwTerminate();
@@ -23,10 +26,24 @@ int main(int argc, char *argv[])
     }
 
     glfwMakeContextCurrent(window);
-
+    
     while(!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_QUADS);
+            glColor4f(0.5, 0.2, 0.4, 1.0);
+            glVertex2f(-0.5, -0.5);
+            glVertex2f(0.5, -0.5);
+            glVertex2f(0.5, 0.5);
+            glVertex2f(-0.5, 0.5);
+        glEnd();
+
+        glBegin(GL_TRIANGLES);
+            glColor4f(0.46, 0.1, 0.7, 0.93);
+            glVertex2f(0, -0.5);
+            glVertex2f(-0.4, 0.5);
+            glVertex2f(0.4, 0.5);
+        glEnd();
 
         glfwSwapBuffers(window);
 
