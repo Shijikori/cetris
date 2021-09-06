83dbd7684872731080f3ae261852e8ac6c0cc29e
Shijikori <jeremi.campagna@gmail.com>
2021-09-06 19:47:50 -0400

    put shape draws in seperate lib

--- app.cpp
+++ app.cpp
@@ -4,47 +4,12 @@
 #include <iostream>
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
+#include "libs/shapes.hpp"
 
 // Window Settings
 int _G_WIDTH = 640;
 int _G_HEIGHT = 480;
 int _G_DEPTH = 3;
-
-// Color struct to store color data
-struct color
-{
-    float red;
-    float green;
-    float blue;
-    float alpha = 1.0;
-    // constructor
-    color(float r, float g, float b, float a=1.0)
-    {
-        red = r;
-        green = g;
-        blue = b;
-        alpha = a;
-    }
-};
-
-// Draw a quad
-void drawQuad(float x, float y, float w, float h, color color)
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_QUADS);
-        glColor4f(color.red, color.green, color.blue, color.alpha);
-        glVertex2f(x, y);
-        glVertex2f(x + w, y);
-        glVertex2f(x + w, y + h);
-        glVertex2f(x, y + h);
-    glEnd();
-}
-
-// Draw a square
-void drawSquare(float x, float y, float size, color color)
-{
-    drawQuad(x,y,size,size,color);
-}
 
 int main(int argc, char *argv[])
 {
